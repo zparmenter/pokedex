5454b612e41244e7d7f48de379fa4dbc62e6116d
zparmenter <zparmenter20@gmail.com>
2022-03-07 14:51:37 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function RandomUser() {
+  const [count, setCounter] = useState(0);
+  const [pokemon, setPokemon] = useState([]);
+  const [sprite, setSprite] = useState(0);
+  const [clickedImage, setClickedImage] = useState('')
+  const [hoverImage, setHoverImage] = useState('')
+  const [activeImage, setActiveImage] = useState(clickedImage)
+  // const [loading, setLoading] = useState(true);
+
+  function spriteUrl(value) {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${clickedImage}${value}.png`;
+  }
+
+  useEffect(async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+    const data = await response.json()
+    const info = data.results;
+    console.log(info);
+    setPokemon(info);
+    setSprite(sprite + 1)
+  }, []);
+
+
+  function generateList(pokemon) {
+    return pokemon.map((pokemon, index) => {
+      return (
+        <div className='grid-item' key={index}>
+          {pokemon.name}
+          <img className='grid-item-img' src={spriteUrl(index + 1)} />
+          <button onClick={() => setClickedImage('')}>Front</button>
+          <button onClick={() => setClickedImage('back/')}>Back</button>
+        </div>
+      )
+    })
+  }
+
+  return(
+    <div>
+      <p>You've clicked {count} times</p>
+
+      <button onClick={() => setCounter(count + 1)}>Click Me</button>
+      <div className='grid-items'>
+        {pokemon.length ? generateList(pokemon) : <div>...loading</div>  }
+      </div>
+    </div>
+  )
+
+}
